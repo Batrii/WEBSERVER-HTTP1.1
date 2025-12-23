8cc4ad134b9f7e33a360b7457371fa33971b304e
Batrii <dhj.nafiai@gmail.com>
2025-12-23 16:31:55 +0100

    begin of the post handler, need to handle each type , and create a json func to check the valid body content

--- .server/run/method/post.cpp
+++ .server/run/method/post.cpp
@@ -4,11 +4,51 @@
 #include <status.hpp>
 #include <time.hpp>
 #include <sys/socket.h>
+#include <response.hpp>
+class urlencoder {
+  private:
+    std::map<std::string, char> _charMap;
+  public:
+    urlencoder();
+};
 
 void methodPost(int client, request& req, ctr& currentServer, long long startRequestTime) {
-  send(client, "HTTP/1.1 204 No Content\r\n\r\n", 25, 0);
-  (void)req;
-  (void)currentServer;
-  (void)startRequestTime;
-  (void)client;
+  if (req.getBody().empty()) {
+    std::map<std::string, std::string> headers;
+    std::string body = "";
+    response res(client, startRequestTime, 400, headers, body, req);
+    res.sendResponse();
+    return;
+  }
+
+  if (req.getHeaders().find("Content-Length") != req.getHeaders().end() && req.getHeaders().at("Content-Length") != "0") {
+    std::string contentType = req.getHeaders().at("Content-Type");
+    if (contentType.find("application/x-www-form-urlencoded") != std::string::npos)
+    {}
+    else if (contentType.find("multipart/form-data") != std::string::npos)
+    {}
+    else if (contentType.find("text/plain") != std::string::npos)
+    {
+      std::string contentstored = req.getBody();
+      std::map<std::string, std::string> headers;
+      std::string body = "Data received:\n" + contentstored;
+      response(client, startRequestTime, 200, headers, body, req).sendResponse();
+      return;
+    }
+    else if (contentType.find("application/json") != std::string::npos)
+    {}
+    else {
+      std::map<std::string, std::string> headers;
+      std::string body = "";
+      response(client, startRequestTime, 415, headers, body, req).sendResponse();
+      return;
+    }
+  }
+  else {
+    std::map<std::string, std::string> headers;
+    std::string body = "";
+    response res(client, startRequestTime, 411, headers, body, req);
+    res.sendResponse();
+    return;
+  }
 }
